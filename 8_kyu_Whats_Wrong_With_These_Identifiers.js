16bc50d4ba5da3ddb4acdea8b23dc3908e1b8bff
lakshmydivyasharma <lakshmydivyasharma@users.noreply.github.com>
2020-09-16 10:47:27 -0400

    daily code challenge

---
+++ 8_kyu_Whats_Wrong_With_These_Identifiers.js
@@ -0,0 +1,21 @@
+https://www.codewars.com/kata/56bb01de0e8b29de50000b19/solutions/javascript
+
+8 kyu
+What's wrong with these identifiers?
+29362% of 5861,818 of 3,435br1anh2 Issues Reported
+JavaScript
+Node v8.1.3
+VIM
+EMACS
+Instructions
+Output
+Past Solutions
+An identifier is simply a name...
+
+
+const Person = {
+  firstname: "John",
+  secondname: "Doe",
+  emailadress: "john.doe@email.com",
+  gender: "M"
+};
