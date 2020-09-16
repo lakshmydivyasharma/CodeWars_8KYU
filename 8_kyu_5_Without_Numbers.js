f5388669da5fa29572e1b2394ec39ff3f362da63
lakshmydivyasharma <lakshmydivyasharma@users.noreply.github.com>
2020-09-16 10:37:57 -0400

    daily code challenge

---
+++ 8_kyu_5_Without_Numbers.js
@@ -0,0 +1,25 @@
+https://www.codewars.com/kata/59441520102eaa25260000bf/train/javascript
+
+8 kyu
+5 without numbers !!
+793790% of 1,4032,008 of 8,616KH!
+JavaScript
+Node v8.1.3
+VIM
+EMACS
+Instructions
+Output
+Write a function that always returns 5
+
+Sounds easy right? Just bear in mind that you can't use any of the following characters: 0123456789*+-/
+
+Good luck :)
+
+FUNDAMENTALS
+
+// count character length for a five letter word
+// ex words: hello, about, above, fjdal, 
+
+function unusualFive() {
+  return ("hello").length
+}
