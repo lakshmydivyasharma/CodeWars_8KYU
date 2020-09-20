2703b184513f61ae26ac480ebfc2663725c1c6c4
lakshmydivyasharma <lakshmydivyasharma@users.noreply.github.com>
2020-09-20 19:37:38 -0400

    daily code challenge

---
+++ 8_kyu_Number_toString.js
@@ -0,0 +1,20 @@
+https://www.codewars.com/kata/53934feec44762736c00044b/train/javascript
+
+8 kyu
+Number toString
+912667% of 1,6315,923 of 15,002YiQi2 Issues Reported
+JavaScript
+Node v8.1.3
+VIM
+EMACS
+Instructions
+Output
+The code gives an error!
+
+a = 123.toString
+Fix it!
+
+BUGSSTRINGS
+
+
+var a = 123..toString();
