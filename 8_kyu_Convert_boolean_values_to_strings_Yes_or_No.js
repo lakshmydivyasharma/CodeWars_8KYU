b6ad5bd7ba711f82196b5fd49332e43252e1322d
lakshmydivyasharma <lakshmydivyasharma@users.noreply.github.com>
2020-09-23 22:01:04 -0400

    daily code challenge

---
+++ 8_kyu_Convert_boolean_values_to_strings_Yes_or_No.js
@@ -0,0 +1,14 @@
+https://www.codewars.com/kata/53369039d7ab3ac506000467/train/javascript
+
+8 kyu
+Convert boolean values to strings 'Yes' or 'No'.
+2859086% of 7,07722,125 of 66,666weavermedia
+JavaScript
+Node v8.1.3
+VIM
+EMACS
+Instructions
+Output
+Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+
+const boolToWord = bool => (bool ? 'Yes' : 'No')
