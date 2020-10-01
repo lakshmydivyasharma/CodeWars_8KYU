059967c481f7d82073e78559f670cbf7aa7495af
lakshmydivyasharma <lakshmydivyasharma@users.noreply.github.com>
2020-10-01 08:40:44 -0400

    daily code challenge

---
+++ 8 kyu Function_3_Multiplying_Two_Numbers.js
@@ -0,0 +1,21 @@
+https://www.codewars.com/kata/523b66342d0c301ae400003b/train/javascript
+8 kyu
+Function 3 - multiplying two numbers
+582376% of 1,8595,796 of 17,688ineiti
+JavaScript
+Node v8.1.3
+VIM
+EMACS
+Instructions
+Output
+Implement a function which multiplies two numbers.
+
+FUNDAMENTALS
+
+first solution: 
+function multiply(num1, num2){
+  return num1 * num2
+}
+
+second solution:
+let multiply = (num1, num2) => num1 * num2;
