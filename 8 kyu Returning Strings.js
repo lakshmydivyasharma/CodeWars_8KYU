b405a76c1d7c8c643bfed76a8fd41e977b8a0c17
lakshmydivyasharma <lakshmydivyasharma@users.noreply.github.com>
2020-10-05 19:54:33 -0400

    daily code challenge

---
+++ 8 kyu Returning Strings.js
@@ -0,0 +1,22 @@
+https://www.codewars.com/kata/55a70521798b14d4750000a4/train/javascript
+
+8 kyu
+Returning Strings
+673186% of 2,4648,170 of 20,349Aweson11 Issue Reported
+JavaScript
+Node v8.1.3
+VIM
+EMACS
+Instructions
+Output
+Make a function that will return a greeting statement that uses an input; your program should return, "Hello, <name> how are you doing today?".
+
+SQL: return results in a column named greeting
+
+[Make sure you type the exact thing I wrote or the program may not execute properly]
+
+FUNDAMENTALSSTRINGS
+
+function greet(name){
+  return "Hello," + " " + name + " " + "how are you doing today?"
+}
