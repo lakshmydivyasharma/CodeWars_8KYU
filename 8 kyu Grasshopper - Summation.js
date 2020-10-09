7f1a890e639c3a021a7b83370f868afe0f30897f
lakshmydivyasharma <lakshmydivyasharma@users.noreply.github.com>
2020-10-09 15:48:32 -0400

    Daily code challenge

---
+++ 8 kyu Grasshopper - Summation.js
@@ -0,0 +1,23 @@
+https://www.codewars.com/kata/55d24f55d7dd296eb9000030/train/javascript
+
+8 kyu
+Grasshopper - Summation
+42112289% of 6,69219,056 of 56,672danleavitt0
+JavaScript
+Node v8.1.3
+VIM
+EMACS
+Instructions
+Output
+Summation
+Write a program that finds the summation of every number from 1 to num. The number will always be a positive integer greater than 0.
+
+For example:
+
+summation(2) -> 3
+1 + 2
+
+summation(8) -> 36
+1 + 2 + 3 + 4 + 5 + 6 + 7 + 8
+
+const summation = n => n * (n + 1) / 2
