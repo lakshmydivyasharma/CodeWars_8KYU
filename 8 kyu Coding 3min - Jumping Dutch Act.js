0e513ea47301de98865fdfa228fa98e70c4f8ff1
lakshmydivyasharma <lakshmydivyasharma@users.noreply.github.com>
2020-10-10 11:46:04 -0400

    daily code challenge

---
+++ 8 kyu Coding 3min - Jumping Dutch Act.js
@@ -0,0 +1,58 @@
+https://www.codewars.com/kata/570bcd9715944a2c8e000009/train/javascript
+
+8 kyu
+Coding 3min : Jumping Dutch act
+351190% of 4211,698 of 1,880myjinxin2015
+JavaScript
+Node v8.1.3
+VIM
+EMACS
+Instructions
+Output
+Coding 3min : Jumping Dutch act
+
+This is the simple version of Shortest Code series. If you need some challenges, please try the challenge version
+
+Task:
+Mr. despair wants to jump off Dutch act, So he came to the top of a building.
+
+Scientific research shows that a man jumped from the top of the roof, when the floor more than 6, the person will often die in an instant; When the floor is less than or equal to 6, the person will not immediately die, he would scream. (without proof)
+
+Input: floor, The height of the building (floor)
+
+Output: a string, The voice of despair(When jumping Dutch act)
+
+Example:
+sc(2) should return "Aa~ Pa! Aa!"
+
+It means:
+
+Mr. despair jumped from the 2 floor, the voice is "Aa~"
+He fell on the ground, the voice is "Pa!"
+He did not die immediately, and the final voice was "Aa!"
+sc(6) should return "Aa~ Aa~ Aa~ Aa~ Aa~ Pa! Aa!"
+
+sc(7) should return "Aa~ Aa~ Aa~ Aa~ Aa~ Aa~ Pa!"
+
+sc(10) should return "Aa~ Aa~ Aa~ Aa~ Aa~ Aa~ Aa~ Aa~ Aa~ Pa!"
+
+if floor<=1, Mr. despair is safe, return ""
+
+The final advice
+Just play in this kata, Don't experiment in real life ;-)
+
+
+Solution 1:
+function sc(floor){
+  if (floor <= 1) return ''
+  if (floor > 6) return `${'Aa~ '.repeat(floor - 1)}Pa!`
+  if (floor <= 6) return `${'Aa~ '.repeat(floor - 1)}Pa! Aa!`
+}
+
+
+Solution 2: 
+  const sc = floor => {
+  if (floor <= 1) return ''
+  if (floor > 6) return `${'Aa~ '.repeat(floor - 1)}Pa!`
+  if (floor <= 6) return `${'Aa~ '.repeat(floor - 1)}Pa! Aa!`
+}
