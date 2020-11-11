7c3a4d824e3864bb239833a63b3b165e78c642f8
lakshmydivyasharma <lakshmydivyasharma@users.noreply.github.com>
2020-11-11 11:36:09 -0500

    daily code challenge

---
+++ 8 kyu Reversed Words.js
@@ -0,0 +1,19 @@
+https://www.codewars.com/kata/51c8991dee245d7ddf00000e/train/javascript
+
+8 kyu
+Reversed Words
+2327788% of 3,1548,889 of 23,957jhoffner1 Issue Reported
+JavaScript
+Node v8.1.3
+VIM
+EMACS
+Instructions
+Output
+Complete the solution so that it reverses all of the words within the string passed in.
+
+NOTES:
+// .split . reverse .join
+
+function reverseWords(str){
+  return str.split(' ').reverse().join(' ');
+}
