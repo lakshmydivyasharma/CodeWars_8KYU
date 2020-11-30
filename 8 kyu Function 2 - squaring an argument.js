07687e391903bb7068270048ed027b4b0ff5a258
lakshmydivyasharma <lakshmydivyasharma@users.noreply.github.com>
2020-11-29 22:44:46 -0500

    Daily code challenge

---
+++ 8 kyu Function 2 - squaring an argument.js
@@ -0,0 +1,12 @@
+https://www.codewars.com/kata/523b623152af8a30c6000027/solutions/javascript
+
+8 kyu
+Function 2 - squaring an argument
+
+solution 1:
+function square(n){
+  return n*n
+}
+
+solution 2:
+const square = (n) => { return n*n };
