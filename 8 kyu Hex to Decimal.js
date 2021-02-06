69baff4ef14d47d6c67e02339675e4b998a3cd74
lakshmydivyasharma <lakshmydivyasharma@users.noreply.github.com>
2021-02-06 00:08:33 -0500

    daily code challenge

---
+++ 8 kyu Hex to Decimal.js
@@ -0,0 +1,15 @@
+8 kyu
+Hex to Decimal
+1454888% of 1,6433,066 of 9,798wichu2 Issues Reported
+JavaScript
+Node v8.1.3
+VIM
+EMACS
+Instructions
+Output
+Complete the function which converts hex number (given as a string) to a decimal number.
+
+
+function hexToDec(hexString){
+ return  parseInt(hexString, 16);
+}
