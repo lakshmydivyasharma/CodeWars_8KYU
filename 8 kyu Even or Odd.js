dfa2e595782342db2876821e9be294f18ebf0ff1
Divya Sharma <60049341+lakshmydivyasharma@users.noreply.github.com>
2022-03-13 14:22:56 -0400

    Update 8 kyu Even or Odd.js

--- 8 kyu Even or Odd.js
+++ 8 kyu Even or Odd.js
@@ -1,3 +1,16 @@
+https://www.codewars.com/kata/53da3dbb4a5168369a0000fe/train/javascript
+
+8 kyu
+Even or Odd
+120033690% of 20,56784,945 of 214,874suuuzi3 Issues Reported
+ JavaScript
+Node v14.x
+VIM
+EMACS
+Instructions
+Output
+Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+
 function even_or_odd(number) {
   if(number % 2 === 0){
     return "Even"
